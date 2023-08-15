80cbe6b870286e7fd18d8f2768188c0080fe4573
BORA040126 <alwaysbediffer@gmail.com>
2023-08-15 16:04:41 +0900

    day1-3

---
+++ day2.py
@@ -0,0 +1,23 @@
+import turtle as t
+
+t.color("red")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+t.color("green")
+t.pensize(3)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+t.color("blue")
+t.circle(50)
---
+++ day3.py
@@ -0,0 +1,7 @@
+for x in range(10):
+    print("Hello!")
+
+for x in range(3):
+    print(100)
+    print(200)
+print(300)
